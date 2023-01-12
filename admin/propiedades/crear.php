6e63013bb1c0f5f7cc1c79fb62c8aaa27df74059
GoinBar <gbarron.ms@gmail.com>
2023-01-12 09:12:11 -0600

    ConectandoConBD
    Signed-off-by: GoinBar <gbarron.ms@gmail.com>

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,4 +1,9 @@
 <?php
+// BASE de datos
+require '../../includes/config/database.php';
+$db = conectarDB();
+// var_dump($db);
+
 require '../../includes/funciones.php';
 incluirTemplate('header');
 ?>
